66e35062b084d4578e15edb87338b0e6ebc66194
ttt1091 <tez1091@yahoo.co.jp>
2021-09-29 20:56:43 +0900

    change

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="stage">
+    <div class="main">
+      <div class="firstView">
+        <div class="hello flex flex-center">
+          <h1>zangiri</h1>
+        </div>
+        <div class="lookDown flex flex-center">
+          <img src="@/assets/icons/arrow_drop_down.svg" alt="">
+        </div>
+      </div>
+      <div>aaa</div>
+    </div>
+    <div class="fingerMenu positionRight-1">
+      <div>
+        <ul class="fingerMenuList">
+          <li>
+            <a class="flex flex-column ta-center" href="">
+              <img src="@/assets/icons/icon-home.svg" alt="">
+              HOME
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
